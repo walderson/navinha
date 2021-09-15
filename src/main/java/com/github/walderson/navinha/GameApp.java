4d7f10961fec31bd48bffaa2c2d2203e0e53ba31
Walderson Shimokawa <lord.caos@uol.com.br>
2021-09-15 19:20:47 -0400

    Variáveis para pontuação e número de vidas

--- src/main/java/com/github/walderson/navinha/GameApp.java
+++ src/main/java/com/github/walderson/navinha/GameApp.java
@@ -1,12 +1,22 @@
 package com.github.walderson.navinha;
 
+import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
+import static com.almasb.fxgl.dsl.FXGL.getGameScene;
+import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
+import static com.almasb.fxgl.dsl.FXGL.getUIFactoryService;
+import static com.almasb.fxgl.dsl.FXGL.getWorldProperties;
+import static com.almasb.fxgl.dsl.FXGL.spawn;
+
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.util.Map;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
+
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 public class GameApp extends GameApplication {
 
@@ -35,8 +45,37 @@ public class GameApp extends GameApplication {
     }
     
     @Override
+    protected void initGameVars(Map<String, Object> vars) {
+    	vars.put("score", 0);
+    	vars.put("lives", 5);
+    }
+    
+    @Override
     protected void initGame() {
-    	FXGL.getGameWorld().addEntityFactory(FACTORY);
-    	this.player = FXGL.spawn("snoopy", 0, 0);
+    	getGameWorld().addEntityFactory(FACTORY);
+    	this.player = spawn("snoopy", 0, 0);
+    }
+    
+    @Override
+    protected void initUI() {
+    	Text scoreLabel = getUIFactoryService().newText("Score", Color.BLACK, 28);
+    	Text scoreValue = getUIFactoryService().newText("", Color.BLACK, 28);
+    	Text livesLabel = getUIFactoryService().newText("Lives", Color.BLACK, 28);
+    	Text livesValue = getUIFactoryService().newText("", Color.BLACK, 28);
+    	
+    	scoreLabel.setTranslateX(20);
+    	scoreLabel.setTranslateY(40);
+    	scoreValue.setTranslateX(100);
+    	scoreValue.setTranslateY(40);
+    	
+    	livesLabel.setTranslateX(getAppWidth() - 160);
+    	livesLabel.setTranslateY(40);
+    	livesValue.setTranslateX(getAppWidth() - 80);
+    	livesValue.setTranslateY(40);
+    	
+    	scoreValue.textProperty().bind(getWorldProperties().intProperty("score").asString());
+    	livesValue.textProperty().bind(getWorldProperties().intProperty("lives").asString());
+    	
+    	getGameScene().addUINodes(scoreLabel, scoreValue, livesLabel, livesValue);
     }
 }
--- src/main/java/com/github/walderson/navinha/component/PlayerComponent.java
+++ src/main/java/com/github/walderson/navinha/component/PlayerComponent.java
@@ -1,5 +1,13 @@
 package com.github.walderson.navinha.component;
 
+import static com.almasb.fxgl.dsl.FXGL.getAppHeight;
+import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
+import static com.almasb.fxgl.dsl.FXGL.getDialogService;
+import static com.almasb.fxgl.dsl.FXGL.getGameController;
+import static com.almasb.fxgl.dsl.FXGL.geti;
+import static com.almasb.fxgl.dsl.FXGL.inc;
+
+import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.component.Component;
 
 import javafx.geometry.Point2D;
@@ -10,5 +18,33 @@ public class PlayerComponent extends Component {
 	@Override
 	public void onUpdate(double tpf) {
 		entity.translate(direction.multiply(1));
+		checkForBounds();
+	}
+
+	private void checkForBounds() {
+		if (entity.getX() < 0) {
+			die();
+		}
+		if (entity.getX() >= getAppWidth()) {
+			die();
+		}
+		if (entity.getY() < 0) {
+			die();
+		}
+		if (entity.getY() >= getAppHeight()) {
+			die();
+		}
+	}
+
+	private void die() {
+		inc("lives", -1);
+		if (geti("lives") <= 0) {
+			getDialogService().showMessageBox("Game Over",
+					() -> getGameController().startNewGame());
+			return;
+		}
+		
+		entity.setPosition(0, 0);
+		direction = new Point2D(1, 1);
 	}
 }
